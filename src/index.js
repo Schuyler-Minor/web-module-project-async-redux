2a3b698680272b46b5f74bbb60ce120b144d8cbc
Schuyler <skym1n0r91@gmail.com>
2022-02-16 17:00:55 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
+
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+//   reducer,
+//   {},
+//   composeEnhancers(applyMiddleware(thunk))
+// );
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </>,
+  document.getElementById("root")
 );
