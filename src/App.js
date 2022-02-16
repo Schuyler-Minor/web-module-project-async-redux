e2ba2cbeb3968f8f563bc892b75e222607a91692
Schuyler <skym1n0r91@gmail.com>
2022-02-16 17:38:17 -0600

    another one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import reducer from "redux";
+
+const initialActivity = [
+  {
+    activity: "Rearrange and organize your room",
+    type: "busywork",
+    participants: 1,
+  },
+];
+const initialState = {
+  activity: initialActivity,
+};
 
 function App() {
   return (
     <div className="App">
       Async Redux Project
+      <h1>What is going on</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,22 +4,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-// import { Provider } from "react-redux";
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "redux-thunk";
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(
-//   reducer,
-//   {},
-//   composeEnhancers(applyMiddleware(thunk))
-// );
-
-ReactDOM.render(
-  <>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
